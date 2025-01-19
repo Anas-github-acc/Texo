8431a01a3a8108b55bf4ca4a8175ad52af93c204
AadiS27 <Aadisinghal0591@gmail.com>
2025-01-19 14:04:24 +0530

    Done with navbar

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -58,9 +58,9 @@ return(
     <div ref={rulerRef} onMouseMove={HandleMouseMove}
     onMouseUp={handleMouseUp}
     onMouseLeave={handleMouseUp}
-    className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+    className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
 
-        <div id="ruler-container" className="max-w-[816px] mx-auto w-full h-full relative">
+        <div id="ruler-container" className=" w-full h-full relative">
             <Marker
                 position={leftMargin}
                 isLeft={true}
@@ -127,20 +127,20 @@ interface MarkerProps{
 const Marker=({position,isLeft,isDragging,onMouseDown,onDoubleClick}:MarkerProps)=>{
     return(
 <div className="absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2"
-style={{ [isLeft?"left":"right"]:`${position}px`}}
-onMouseDown={onMouseDown}
-onDoubleClick={onDoubleClick}>
-<FaCaretDown className='absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2'/>
-<div 
-className='absolute left-1/2 top-4 transform -translate-x-1/2 transition-opacity '
-style={{
-    height:"100vh",
-    width:"1px",
-    transform:"scaleX(0.5)",
-    backgroundColor:"#3b72f6",
-    display:isDragging?"block":"none",
-}}
-/>
+    style={{ [isLeft ? "left" : "right"]: `${position}px` }}
+    onMouseDown={onMouseDown}
+    onDoubleClick={onDoubleClick}>
+    <FaCaretDown className='absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2' />
+    <div
+        className='absolute left-1/2 top-4 transform -translate-x-1/2'
+        style={{
+            height: "100vh",
+            width: "1px",
+            transform: "scaleX(1)",
+            backgroundColor: "#3b72f6",
+            display: isDragging ? "block" : "none",
+        }}
+    />
 </div>
 )
 }
